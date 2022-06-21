52e1a0480dd0395d2b2e4b957b5062e016b2738b
santiiagoduarte <100471920+santiiagoduarte@users.noreply.github.com>
2022-06-20 22:50:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let entrada = prompt("Ingresa el numero mágico!");
+
+while(entrada != "31" ){
+    alert("El número ingrsado fue "+ entrada);
+    entrada = prompt("Número incorrecto, ingresa otro valor");
+}
